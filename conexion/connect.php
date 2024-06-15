3b6f914419f40142bf84496c539455e01051459c
Nachociro <ifigueroaferrari@gmail.com>
2024-06-15 15:23:39 -0300

    correccion final de alta cliente

--- conexion/connect.php
+++ conexion/connect.php
@@ -8,9 +8,6 @@ $puerto= '3306';
 
 // hay que poner los cambios segun el host que usemos por ahora esta esto para usarlo desde el xamp
 
-
-
-
 $con = mysqli_connect($servidor,$usuario,$contrasena,$base_de_datos,$puerto);
 if (!$con) {
     die("Error al conectar: " . mysqli_connect_error());
