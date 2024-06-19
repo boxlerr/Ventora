64535742f8909b323995c34b8d717254b0a0dae2
ErosB8 <eros0088@hotmail.com>
2024-06-19 11:51:40 -0300

    Reapply "Arreglos admin"
    This reverts commit 1b313b8ae57d6e62a6eedca72730e810347c17bf.

---
+++ 5.HTML_Template_Dashboard/footer_admin.php
@@ -0,0 +1,10 @@
+<div class="row pb-30 pt-15">
+    <div class="col-sm-6">
+        <script>
+            document.write(new Date().getFullYear())
+        </script> &copy;, Ecom - HTML Ecommerce Template .
+    </div>
+    <div class="col-sm-6">
+        <div class="text-sm-end">All rights reserved</div>
+    </div>
+</div>
