a99dea7e9d2fa59923185999ab4762cb685f9913
Nachociro <ifigueroaferrari@gmail.com>
2024-06-21 12:01:39 -0300

    page-marcas

--- 5.HTML_Template_Dashboard/page-brands.php
+++ 5.HTML_Template_Dashboard/page-brands.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="es">
-
 <head>
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
@@ -14,11 +13,12 @@
   <link href="assets/css/style.css" rel="stylesheet">
   <title>Ecom - Marketplace Dashboard Template</title>
 </head>
-
 <body>
   <?php
-  include_once("header_admin.php")
+  include_once("header_admin.php");
+  include_once("../conexion/connect.php");
   ?>
+  
   <section class="content-main">
     <div class="content-header">
       <div>
@@ -33,180 +33,89 @@
           <div class="col-lg-4 mb-lg-0 mb-15 me-auto">
             <input class="form-control" type="text" placeholder="Search...">
           </div>
-          <div class="col-lg-2 col-6">
-            <div class="custom_select">
-              <select class="form-select select-nice">
-                <option selected="">Categories</option>
-                <option>Technology</option>
-                <option>Fashion</option>
-                <option>Home Decor</option>
-                <option>Healthy</option>
-                <option>Travel</option>
-                <option>Auto-car</option>
-              </select>
-            </div>
+          <!-- categorías -->
+            
+                <div class="col-lg-2 col-6 col-md-3">
+            <select class="form-select">
+              <option>All category</option>
+              <?php
+              // Obtener categorías desde la base de datos
+              $sql_categorias = "SELECT nombre FROM categoria";
+              $result_categorias = $con->query($sql_categorias);
+
+              if ($result_categorias->num_rows > 0) {
+                  while ($row = $result_categorias->fetch_assoc()) {
+                      $categoria_nombre = htmlspecialchars($row['nombre']);
+                      echo '<option>' . $categoria_nombre . '</option>';
+                  }
+              }
+              ?>
+            </select>
           </div>
+            
+          
+
           <div class="col-lg-2 col-6">
             <input class="form-control" type="date" name="">
           </div>
         </div>
       </header>
+
       <div class="card-body">
         <div class="row gx-3">
-          <div class="col-xl-2 col-lg-3 col-md-4 col-6">
-            <figure class="card border-1">
-              <div class="card-header bg-white text-center"><img class="img-fluid" height="76" src="assets/imgs/brands/brand-1.jpg" alt="Logo"></div>
-              <figcaption class="card-body text-center">
-                <h6 class="card-title m-0">Cardinal</h6><a href="#"> 398 items</a>
-              </figcaption>
-            </figure>
-          </div>
-          <div class="col-xl-2 col-lg-3 col-md-4 col-6">
-            <figure class="card border-1">
-              <div class="card-header bg-white text-center"><img class="img-fluid" height="76" src="assets/imgs/brands/brand-2.jpg" alt="Logo"></div>
-              <figcaption class="card-body text-center">
-                <h6 class="card-title m-0">BirdFly</h6><a href="#"> 13 items</a>
-              </figcaption>
-            </figure>
-          </div>
-          <div class="col-xl-2 col-lg-3 col-md-4 col-6">
-            <figure class="card border-1">
-              <div class="card-header bg-white text-center"><img class="img-fluid" height="76" src="assets/imgs/brands/brand-3.jpg" alt="Logo"></div>
-              <figcaption class="card-body text-center">
-                <h6 class="card-title m-0">Cocorico</h6><a href="#"> 13 items</a>
-              </figcaption>
-            </figure>
-          </div>
-          <div class="col-xl-2 col-lg-3 col-md-4 col-6">
-            <figure class="card border-1">
-              <div class="card-header bg-white text-center"><img class="img-fluid" height="76" src="assets/imgs/brands/brand-4.jpg" alt="Logo"></div>
-              <figcaption class="card-body text-center">
-                <h6 class="card-title m-0">Yogilist</h6><a href="#"> 87 items</a>
-              </figcaption>
-            </figure>
-          </div>
-          <div class="col-xl-2 col-lg-3 col-md-4 col-6">
-            <figure class="card border-1">
-              <div class="card-header bg-white text-center"><img class="img-fluid" height="76" src="assets/imgs/brands/brand-5.jpg" alt="Logo"></div>
-              <figcaption class="card-body text-center">
-                <h6 class="card-title m-0">Acerie</h6><a href="#"> 10 items</a>
-              </figcaption>
-            </figure>
-          </div>
-          <div class="col-xl-2 col-lg-3 col-md-4 col-6">
-            <figure class="card border-1">
-              <div class="card-header bg-white text-center"><img class="img-fluid" height="76" src="assets/imgs/brands/brand-6.jpg" alt="Logo"></div>
-              <figcaption class="card-body text-center">
-                <h6 class="card-title m-0">Shivakin</h6><a href="#"> 398 items</a>
-              </figcaption>
-            </figure>
-          </div>
-          <div class="col-xl-2 col-lg-3 col-md-4 col-6">
-            <figure class="card border-1">
-              <div class="card-header bg-white text-center"><img class="img-fluid" height="76" src="assets/imgs/brands/brand-7.jpg" alt="Logo"></div>
-              <figcaption class="card-body text-center">
-                <h6 class="card-title m-0">Acera</h6><a href="#"> 398 items</a>
-              </figcaption>
-            </figure>
-          </div>
-          <div class="col-xl-2 col-lg-3 col-md-4 col-6">
-            <figure class="card border-1">
-              <div class="card-header bg-white text-center"><img class="img-fluid" height="76" src="assets/imgs/brands/brand-8.jpg" alt="Logo"></div>
-              <figcaption class="card-body text-center">
-                <h6 class="card-title m-0">Lion electronics</h6><a href="#"> 398 items</a>
-              </figcaption>
-            </figure>
-          </div>
-          <div class="col-xl-2 col-lg-3 col-md-4 col-6">
-            <figure class="card border-1">
-              <div class="card-header bg-white text-center"><img class="img-fluid" height="76" src="assets/imgs/brands/brand-9.jpg" alt="Logo"></div>
-              <figcaption class="card-body text-center">
-                <h6 class="card-title m-0">TwoHand</h6><a href="#"> 398 items</a>
-              </figcaption>
-            </figure>
-          </div>
-          <div class="col-xl-2 col-lg-3 col-md-4 col-6">
-            <figure class="card border-1">
-              <div class="card-header bg-white text-center"><img class="img-fluid" height="76" src="assets/imgs/brands/brand-10.jpg" alt="Logo"></div>
-              <figcaption class="card-body text-center">
-                <h6 class="card-title m-0">Kiaomin</h6><a href="#"> 398 items</a>
-              </figcaption>
-            </figure>
-          </div>
-          <div class="col-xl-2 col-lg-3 col-md-4 col-6">
-            <figure class="card border-1">
-              <div class="card-header bg-white text-center"><img class="img-fluid" height="76" src="assets/imgs/brands/brand-11.jpg" alt="Logo"></div>
-              <figcaption class="card-body text-center">
-                <h6 class="card-title m-0">Nokine</h6><a href="#"> 398 items</a>
-              </figcaption>
-            </figure>
-          </div>
-          <div class="col-xl-2 col-lg-3 col-md-4 col-6">
-            <figure class="card border-1">
-              <div class="card-header bg-white text-center"><img class="img-fluid" height="76" src="assets/imgs/brands/brand-12.jpg" alt="Logo"></div>
-              <figcaption class="card-body text-center">
-                <h6 class="card-title m-0">Company name</h6><a href="#"> 13 items</a>
-              </figcaption>
-            </figure>
-          </div>
-          <div class="col-xl-2 col-lg-3 col-md-4 col-6">
-            <figure class="card border-1">
-              <div class="card-header bg-white text-center"><img class="img-fluid" height="76" src="assets/imgs/brands/brand-13.jpg" alt="Logo"></div>
-              <figcaption class="card-body text-center">
-                <h6 class="card-title m-0">Company name</h6><a href="#"> 398 items</a>
-              </figcaption>
-            </figure>
-          </div>
-          <div class="col-xl-2 col-lg-3 col-md-4 col-6">
-            <figure class="card border-1">
-              <div class="card-header bg-white text-center"><img class="img-fluid" height="76" src="assets/imgs/brands/brand-14.jpg" alt="Logo"></div>
-              <figcaption class="card-body text-center">
-                <h6 class="card-title m-0">Company name</h6><a href="#"> 13 items</a>
-              </figcaption>
-            </figure>
-          </div>
-          <div class="col-xl-2 col-lg-3 col-md-4 col-6">
-            <figure class="card border-1">
-              <div class="card-header bg-white text-center"><img class="img-fluid" height="76" src="assets/imgs/brands/brand-15.jpg" alt="Logo"></div>
-              <figcaption class="card-body text-center">
-                <h6 class="card-title m-0">Company name</h6><a href="#"> 398 items</a>
-              </figcaption>
-            </figure>
-          </div>
-          <div class="col-xl-2 col-lg-3 col-md-4 col-6">
-            <figure class="card border-1">
-              <div class="card-header bg-white text-center"><img class="img-fluid" height="76" src="assets/imgs/brands/brand-16.jpg" alt="Logo"></div>
-              <figcaption class="card-body text-center">
-                <h6 class="card-title m-0">Company name</h6><a href="#"> 13 items</a>
-              </figcaption>
-            </figure>
-          </div>
-          <div class="col-xl-2 col-lg-3 col-md-4 col-6">
-            <figure class="card border-1">
-              <div class="card-header bg-white text-center"><img class="img-fluid" height="76" src="assets/imgs/brands/brand-17.jpg" alt="Logo"></div>
-              <figcaption class="card-body text-center">
-                <h6 class="card-title m-0">Company name</h6><a href="#"> 13 items</a>
-              </figcaption>
-            </figure>
-          </div>
-          <div class="col-xl-2 col-lg-3 col-md-4 col-6">
-            <figure class="card border-1">
-              <div class="card-header bg-white text-center"><img class="img-fluid" height="76" src="assets/imgs/brands/brand-18.jpg" alt="Logo"></div>
-              <figcaption class="card-body text-center">
-                <h6 class="card-title m-0">Company name</h6><a href="#"> 13 items</a>
-              </figcaption>
-            </figure>
-          </div>
+        <?php
+        // Consulta para obtener las marcas y la cantidad de productos
+        $sql = "
+            SELECT 
+                marca.marca_id, 
+                marca.nombre_marca, 
+                marca.logo_marca, 
+                COUNT(producto.producto_id) AS cantidad_productos 
+            FROM 
+                marca 
+            LEFT JOIN 
+                producto 
+            ON 
+                marca.marca_id = producto.marca_id 
+            GROUP BY 
+                marca.marca_id, 
+                marca.nombre_marca, 
+                marca.logo_marca
+        ";
+
+        $result = $con->query($sql);
+        ?>
+
+        <?php
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                echo '
+                <div class="col-xl-2 col-lg-3 col-md-4 col-6">
+                    <figure class="card border-1">
+                        <div class="card-header bg-white text-center">
+                            <img class="img-fluid" height="76" src="assets/imgs/brands/'.$row["logo_marca"].'" alt="Logo">
+                        </div>
+                        <figcaption class="card-body text-center">
+                            <h6 class="card-title m-0">'.$row["nombre_marca"].'</h6>
+                            <a href="#"> '.$row["cantidad_productos"].' items</a>
+                        </figcaption>
+                    </figure>
+                </div>';
+            }
+        } else {
+            echo "No se encontraron marcas.";
+        }
+        $con->close();
+        ?>
         </div>
       </div>
     </div>
   </section>
   <footer class="main-footer font-xs">
     <?php
-    include_once("header_admin.php")
+    include_once("footer_admin.php"); 
     ?>
   </footer>
-  </main>
   <script src="assets/js/vendors/jquery-3.6.0.min.js"></script>
   <script src="assets/js/vendors/bootstrap.bundle.min.js"></script>
   <script src="assets/js/vendors/select2.min.js"></script>
@@ -216,5 +125,4 @@
   <script src="assets/js/main.js?v=1.0.0"></script>
   <script src="assets/js/custom-chart.js" type="text/javascript"></script>
 </body>
-
 </html>
--- 5.HTML_Template_Dashboard/registros/logout_administrador.php
+++ 5.HTML_Template_Dashboard/registros/logout_administrador.php
@@ -5,7 +5,6 @@ session_start();
 // Limpiar todas las variables de sesión
 $_SESSION = array();
 
-// Si se desea destruir la sesión completamente, también se deben destruir las cookies de sesión.
 if (ini_get("session.use_cookies")) {
     $params = session_get_cookie_params();
     setcookie(session_name(), '', time() - 42000,
@@ -14,7 +13,6 @@ if (ini_get("session.use_cookies")) {
     );
 }
 
-// Finalmente, destruir la sesión
 session_destroy();
 
 // Redirigir al índice
