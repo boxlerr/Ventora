73dedb5773e329b93e1110453f8f3687139ad60f
ErosB8 <eros0088@hotmail.com>
2024-06-21 15:25:38 -0300

    Arreglos Footer

--- 1.HTML_Template_Frontend_LTR/footer.php
+++ 1.HTML_Template_Frontend_LTR/footer.php
@@ -2,52 +2,42 @@
     <div class="container">
         <div class="row">
         <div class="col-lg-3 width-25 mb-30">
-            <h4 class="mb-30 color-gray-1000">Contact</h4>
-            <div class="font-md mb-20 color-gray-900"><strong class="font-md-bold">Address:</strong> 502 New Design Str, Melbourne, San Francisco, CA 94110, United States</div>
-            <div class="font-md mb-20 color-gray-900"><strong class="font-md-bold">Phone:</strong> (+01) 123-456-789</div>
+            <h4 class="mb-30 color-gray-1000">Contacto</h4>
+            <div class="font-md mb-20 color-gray-900"><strong class="font-md-bold">Dirección:</strong> 502 New Design Str, Melbourne, San Francisco, CA 94110, United States</div>
+            <div class="font-md mb-20 color-gray-900"><strong class="font-md-bold">Teléfono:</strong> (+01) 123-456-789</div>
             <div class="font-md mb-20 color-gray-900"><strong class="font-md-bold">E-mail:</strong> contact@ecom-market.com</div>
-            <div class="font-md mb-20 color-gray-900"><strong class="font-md-bold">Hours:</strong> 8:00 - 17:00, Mon - Sat</div>
             <div class="mt-30"><a class="icon-socials icon-facebook" href="#"></a><a class="icon-socials icon-instagram" href="#"></a><a class="icon-socials icon-twitter" href="#"></a><a class="icon-socials icon-linkedin" href="#"></a></div>
         </div>
         <div class="col-lg-3 width-20 mb-30">
-            <h4 class="mb-30 color-gray-1000">Make Money with Us</h4>
+            <h4 class="mb-30 color-gray-1000">Servicios</h4>
             <ul class="menu-footer">
-            <li><a href="page-about-us.php">Mission &amp; Vision</a></li>
-            <li><a href="page-about-us.php">Our Team</a></li>
-            <li><a href="page-careers.html">Careers</a></li>
-            <li><a href="#">Press &amp; Media</a></li>
-            <li><a href="#">Advertising</a></li>
-            <li><a href="#">Testimonials</a></li>
+            <li><a href="importar.php">Importá con nosotros</a></li>
+            <li><a href="exportar.php">Exportá con nosotros</a></li>
+            <li><a href="#">Testimonios</a></li>
             </ul>
         </div>
         <div class="col-lg-3 width-16 mb-30">
-            <h4 class="mb-30 color-gray-1000">Company</h4>
+            <h4 class="mb-30 color-gray-1000">Empresa</h4>
             <ul class="menu-footer">
-            <li><a href="blog-2.html">Our Blog</a></li>
-            <li><a href="#">Plans &amp; Pricing</a></li>
-            <li><a href="#">Knowledge Base</a></li>
-            <li><a href="#">Cookie Policy</a></li>
-            <li><a href="#">Office Center</a></li>
-            <li><a href="blog.html">News &amp; Events</a></li>
+            <li><a href="page-about-us.php">Sobre nosotros</a></li>
+            <li><a href="politicas.php">Políticas de privacidad</a></li>
+            <li><a href="page-term.php">Términos y condiciones</a></li>
+            <li><a href="#">Cookies</a></li>
+            <!--<li><a href="#">Rastrear envío</a></li>-->
             </ul>
         </div>
         <div class="col-lg-3 width-16 mb-30">
-            <h4 class="mb-30 color-gray-1000">My account</h4>
+            <h4 class="mb-30 color-gray-1000">Mi cuenta</h4>
             <ul class="menu-footer">
+            <li><a href="page-account.php">Configuración</a></li>
             <li><a href="#">FAQs</a></li>
-            <li><a href="#">Editor Help</a></li>
-            <li><a href="#">Community</a></li>
-            <li><a href="#">Live Chatting</a></li>
-            <li><a href="page-contact.php">Contact Us</a></li>
-            <li><a href="#">Support Center</a></li>
+            <li><a href="page-contact.php">Contáctanos</a></li>
             </ul>
         </div>
         <div class="col-lg-3 width-23">
-            <h4 class="mb-30 color-gray-1000">App &amp; Payment</h4>
+            <h4 class="mb-30 color-gray-1000">Medios de pago</h4>
             <div>
-            <p class="font-md color-gray-900">Download our Apps and get extra 15% Discount on your first Order&mldr;!</p>
-            <div class="mt-20"><a class="mr-10" href="#"><img src="assets/imgs/template/appstore.png" alt="Ecom"></a><a href="#"><img src="assets/imgs/template/google-play.png" alt="Ecom"></a></div>
-            <p class="font-md color-gray-900 mt-20 mb-10">Secured Payment Gateways</p><img src="assets/imgs/template/payment-method.png" alt="Ecom">
+            <p class="font-md color-gray-900 mt-20 mb-10">Aceptamos:</p><img src="assets/imgs/template/payment-method.png" alt="Ecom">
             </div>
         </div>
         </div>
