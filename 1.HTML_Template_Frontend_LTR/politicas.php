975d3821cc084000d2b253995a83ec8335b03d56
boxlerr <boxlerjulian@hotmail.com>
2024-06-27 13:50:18 -0300

    Politicas
    Politicas privacidad, devolucion, terminos y condiciones, faqs terminados (cambiar banner, agregar direccion ale)

--- 1.HTML_Template_Frontend_LTR/politicas.php
+++ 1.HTML_Template_Frontend_LTR/politicas.php
@@ -27,10 +27,91 @@
         <div class="row">
           <div class="col-lg-10 mx-auto page-content">
             <h2 class="text-center mb-20">Politicas de Privacidad</h2><img class="mb-30" src="assets/imgs/page/about/team.jpg" alt="Ecom">
+            <img src="assets/imgs/img/privacidad-icon.webp" alt="Icono devolucion vestore" class="imagen-pequena">
+            <h5>1. Recolección de Datos Personales.</h5>
+<p><strong>1.1 Definición de datos personales</strong></p>
+<p>Datos personales significa cualquier información relacionada con una persona física identificada o identificable (‘sujeto de datos’); una persona física identificable es aquella que puede ser identificada, directa o indirectamente, en particular mediante un identificador como un nombre, un número de identificación, datos de localización. Los datos personales no incluyen datos que han sido anonimados irreversiblemente o agregados para que no nos permitan identificarte, ya sea en combinación con otra información o de otra manera.</p>
+<p>A continuación se describe los tipos de datos personales que podemos recopilar y cómo podemos utilizarlos:</p>
+<p><strong>1.2 Qué Datos Personales Recopilamos</strong></p>
+<p>Dependiendo de los productos y servicios que elijas, recopilamos diferentes tipos de datos personales tuyos o sobre ti.</p>
+<p><strong>(a) Datos que proporcionas.</strong> Recopilamos los datos personales que proporcionas cuando creas una cuenta para usar nuestros productos y servicios o interactúas con nosotros, como cuando completas información de la cuenta, participas en nuestras encuestas en línea o promociones, dejas comentarios sobre los productos y utilizas nuestra herramienta de ayuda en línea o chat en línea o nos envías un correo electrónico.</p>
+<p><strong>(b) Datos de tu equipo terminal.</strong></p>
+<p><strong>Fotos/Medios/Archivos.</strong></p>
+<p>Después de obtener tu permiso, el acceso a Fotos/Medios/Archivos puede incluir la capacidad de:</p>
+<ul>
+<li>Leer el contenido de tu almacenamiento USB (como tarjeta SD).</li>
+<li>Agregar contenido a tu almacenamiento USB.</li>
+<li>Formatear el almacenamiento externo.</li>
+<li>Montar o desmontar el almacenamiento externo.</li>
+</ul>
+<p><strong>Almacenamiento local.</strong></p>
+<p>Podemos recopilar y almacenar información (incluidos datos personales) localmente en tu dispositivo mediante mecanismos como el almacenamiento web del navegador (incluido HTML 5) y cachés de datos de la aplicación.</p>
+<p><strong>Cámara.</strong> Puedes subir fotos cuando comentas a través de servicios en línea. En este caso, necesitamos recopilar imágenes y otra información de tu dispositivo.</p>
+<p>Números de aplicación únicos. Ciertos servicios incluyen un número de aplicación único. Estos números y la información relacionada con tu instalación (por ejemplo, el tipo de sistema operativo y el número de versión de la aplicación) pueden ser enviados al sitio web cuando instalas o desinstalas esos servicios o cuando esos servicios contactan periódicamente a nuestros servidores (por ejemplo, para actualizaciones automáticas).</p>
+<p><strong>(c) Datos sobre la compra.</strong></p>
+<p>Si realizas una compra, recopilamos datos personales relacionados con la compra, incluidos el producto que compraste, el número de transacción, nombre de usuario, dirección de envío y otra información de autenticación, así como detalles de facturación y contacto.</p>
+<p><strong>(d) Datos sobre la mejora de nuestros servicios y productos.</strong> Cuando visitas nuestros sitios web, podemos recopilar datos sobre el tipo de dispositivo que utilizas, el identificador único de tu dispositivo, la dirección IP de tu dispositivo, tu sistema operativo, el tipo de navegador de Internet que utilizas, información de uso, información de diagnóstico y datos de ubicación de o sobre las computadoras, teléfonos u otros dispositivos en los que instalas o accedes a nuestros productos o servicios. Donde esté disponible, nuestros servicios pueden utilizar GPS, tu dirección IP y otras tecnologías para determinar la ubicación aproximada de un dispositivo para permitirnos mejorar nuestros productos y servicios.</p>
+<p><strong>1.3 Cuál es la Fuente de tus Datos Personales</strong></p>
+<p>Con tu consentimiento previo expreso, recopilamos tus datos personales:</p>
+<p>(a) Directamente de ti según lo descrito anteriormente, incluida la información generada cuando utilizas nuestros servicios y productos;</p>
+<p>(b) De tu equipo terminal según lo descrito anteriormente;</p>
+<p>(c) De nuestros socios. Son terceros como Google, Facebook, Twitter, VK, etc. Cuando inicias sesión en nuestros sitios web a través de estas cuentas, podemos obtener tu información de inicio de sesión automáticamente porque estos terceros nos han autorizado previamente a recopilar tu información para ayudarte a iniciar sesión convenientemente.</p>
+<p>(d) De Cookies y Tecnologías Similares. Cuando visitas nuestro sitio web, recopilamos automáticamente información técnica como tu dirección IP mediante cookies.</p>
+<h5>2. Cómo Utilizamos Cookies y Tecnologías Similares.</h5>
+<p>Utilizamos cookies y tecnologías similares para proporcionar, proteger y mejorar nuestros productos y servicios, como personalizar contenido, ofrecer y medir anuncios, comprender el comportamiento del usuario y proporcionar una experiencia más segura. Puedes eliminar o rechazar cookies utilizando la configuración de tu navegador o dispositivo, pero en algunos casos, hacerlo puede afectar tu capacidad para usar nuestros productos y servicios.</p>
+<p><strong>2.1 Definición de "Cookies"</strong></p>
+<p>Las cookies son pequeños archivos de texto que los sitios web que visitas colocan en tu dispositivo. Las cookies realizan varias funciones, como recordar tus preferencias y elementos elegidos, ayudarte a mejorar tu experiencia en el sitio y tratar de garantizar que los anuncios u ofertas que ves en línea sean más relevantes para ti. En sí mismas, las cookies son inofensivas y cumplen funciones cruciales para los sitios web. Estas "cookies" pueden dividirse en 4 tipos, cada uno de los cuales se describe a continuación.</p>
+<p><strong>2.2 Tipos de Cookies</strong></p>
+<p>(a) Cookies estrictamente necesarias.</p>
+<p>Estas cookies son esenciales para que puedas navegar por nuestro sitio web y utilizar sus servicios fundamentales, como visitar nuestro sitio web o realizar una compra.</p>
+<p>Estas cookies no requieren tu consentimiento. Si deshabilitas estas cookies, no podremos cumplir con tu solicitud básica.</p>
+<p>(b) Cookies de estadísticas.</p>
+<p>Estas cookies recopilan información anónima sobre cómo las personas usan el sitio y los datos se combinan con otros usuarios para permitirnos mejorar el funcionamiento del sitio. Esto incluye cookies de servicios de análisis de terceros que son para el uso exclusivo del propietario del sitio web visitado. Por ejemplo, utilizamos cookies de Google Analytics para ayudarnos a comprender cómo los clientes llegan a nuestro sitio, navegan o usan nuestro sitio y destacar áreas donde podemos mejorar aspectos como la navegación, la experiencia de compra y las campañas de marketing.</p>
+<p>Los datos almacenados por estas cookies nunca muestran detalles personales que puedan establecer tu identidad individual. Si deshabilitas o rechazas estas cookies, es posible que no puedas usar ciertas características de nuestros sitios web y servicios, y puede reducir el soporte o la información que podemos proporcionarte.</p>
+<p>(c) Cookies de preferencias.</p>
+<p>Estas cookies permiten a nuestro sitio recordar las elecciones que has hecho sobre tus preferencias, como el idioma que prefieres, o cuál es tu nombre de usuario y contraseña para que puedas iniciar sesión automáticamente y podamos ofrecerte mejores servicios y mejorar tu experiencia de compra. Si rechazas estas cookies, es posible que sientas que la eficiencia de navegación web ha disminuido.</p>
+<p>(d) Cookies de marketing.</p>
+<p>Estas cookies rastrean información sobre tus hábitos de navegación para recomendarte publicidad más personalizada. Estas cookies pueden recordar tus actividades en línea y/o en sitios o servicios en línea de terceros, y compartirlas con terceros como anunciantes. Por ejemplo, al colocar estas cookies, puedes compartir páginas y contenido en nuestros sitios web y servicios a través de redes sociales y otros sitios web de terceros. Pero solo permitiremos cookies de terceros en nuestros sitios web con tu consentimiento explícito. Para entender cómo utilizan cookies los terceros, revisa sus políticas de privacidad. Si rechazas estas cookies, también verás anuncios que no son tan relevantes para ti, lo que puede afectar tu experiencia de compra.</p>
+<p><strong>2.3 Cómo controlar la configuración de cookies</strong></p>
+<p>Cuando ingresas por primera vez al sitio web, solicitamos tu consentimiento para las cookies a través de un banner de cookies. Puedes seleccionar los tipos de cookies que te convengan. Ten en cuenta que si eliges rechazar las cookies, tu acceso a ciertas características puede estar restringido.</p>
+<p>Puedes eliminar o rechazar cookies utilizando la configuración de tu navegador o dispositivo. La mayoría de los navegadores web aceptan cookies automáticamente, pero puedes modificar la configuración de tu navegador visitando la sección de Ayuda de la barra de herramientas de tu navegador. Tu dispositivo móvil también puede permitirte controlar las cookies a través de su configuración. Para obtener más detalles, consulta las instrucciones del fabricante del dispositivo.</p>
+<p>Vale la pena mencionar que es posible que debas volver a seleccionar las cookies en nuestros sitios web después de borrar la caché de cookies.</p>
+<p><strong>2.4 Otras tecnologías similares</strong></p>
+<p>DPAPI (Data Protection Application Programming Interface) es una interfaz de programación de aplicaciones criptográficas simple disponible como un componente integrado en Windows 2000 y versiones posteriores de los sistemas operativos Microsoft Windows. En teoría, la API de Protección de Datos puede habilitar el cifrado simétrico de cualquier tipo de datos; en la práctica, su uso principal en el sistema operativo Windows es realizar el cifrado simétrico de claves criptográficas privadas, utilizando una clave derivada de la contraseña de Windows del usuario.</p>
+<p>HTML5 Web Storage es una mejor solución que las cookies. Web Storage, o Almacenamiento Web, permite a los sitios web almacenar datos dentro del navegador en el dispositivo del usuario. Las API de almacenamiento web son seguras y, a diferencia de las cookies, pueden almacenar una gran cantidad de datos. Se guardan en el dispositivo del usuario, y puede acceder a ellos cualquier script en la misma fuente.</p>
+<h5>3. Cómo Utilizamos tus Datos Personales.</h5>
+<p><strong>3.1 Procesamiento de tus datos personales</strong></p>
+<p>Podemos procesar tus datos personales para los fines descritos en esta política de privacidad con tu consentimiento previo, para cumplir con obligaciones legales o cuando tengamos una evaluación de que hacerlo es necesario para proteger nuestros intereses legítimos o los de un tercero a quien podamos divulgar la información.</p>
+<p><strong>3.2 Fines de uso de datos personales</strong></p>
+<p>(a) Crear y mantener tu cuenta de usuario.</p>
+<p>(b) Proveer, mejorar y desarrollar nuestros productos y servicios.</p>
+<p>(c) Procesar tus transacciones de compra, procesar tus pedidos y entregarte productos, procesar pagos o cualquier otra transacción relacionada.</p>
+<p>(d) Comunicarnos contigo y responder a tus consultas o solicitudes.</p>
+<p>(e) Ofrecerte soporte técnico y de atención al cliente.</p>
+<p>(f) Prevenir y perseguir el fraude y realizar investigaciones relacionadas con el fraude.</p>
+<p>(g) Cumplir con requisitos legales y reglamentarios.</p>
+<p>(h) Entender tu comportamiento en nuestros sitios web y servicios y personalizar nuestro marketing y publicidad.</p>
+<p>(i) Mejorar la seguridad y proteger los derechos y la propiedad de los usuarios y de terceros.</p>
+<h5>4. Cómo Compartimos y Divulgamos tus Datos Personales.</h5>
+<p>Solo compartimos tus datos personales de la manera descrita en esta política de privacidad. Podemos compartir tus datos personales con:</p>
+<p><strong>4.1 Proveedores de servicios.</strong> Proveedores de servicios externos que procesan datos personales en nuestro nombre, como empresas de análisis de datos, proveedores de servicios de marketing, servicios de alojamiento y almacenamiento, servicios de atención al cliente, etc.</p>
+<p><strong>4.2 Socios comerciales.</strong> Socios comerciales con quienes ofrecemos productos y servicios conjuntamente.</p>
+<p><strong>4.3 Cumplimiento de la ley y seguridad.</strong> En caso de una emergencia, si estamos obligados por ley a hacerlo, o si creemos que es necesario para proteger nuestros derechos y los derechos de nuestros usuarios y del público.</p>
+<h5>5. Seguridad de tus Datos Personales.</h5>
+<p>Utilizamos medidas técnicas y organizativas adecuadas para proteger tus datos personales contra el acceso no autorizado, la pérdida o la destrucción. Sin embargo, ningún sistema es completamente seguro y no podemos garantizar la seguridad de tus datos en todo momento.</p>
+<h5>6. Retención de Datos Personales.</h5>
+<p>Retenemos tus datos personales solo el tiempo necesario para los fines descritos en esta política de privacidad o según lo requiera la ley.</p>
+<h5>7. Tus Derechos.</h5>
+<p>Tienes derecho a acceder, rectificar o eliminar tus datos personales, así como a restringir u oponerte al procesamiento de tus datos personales. También tienes derecho a la portabilidad de los datos.</p>
+<p>Si tienes alguna pregunta sobre esta política de privacidad o quieres ejercer tus derechos, puedes contactarnos a través de la información proporcionada en nuestro sitio web.</p>
+<p>Nos reservamos el derecho de modificar esta política de privacidad en cualquier momento, por lo que te recomendamos revisarla periódicamente.</p>
+              <p></p>
+              <h5>Gracias, somos Vestore.</h5>
           </div>
         </div>
       </div>
     </section>
+    <!--- 
     <section class="section-box mt-90 mb-50">
       <div class="container">
         <ul class="list-col-5">
@@ -100,6 +181,7 @@
         </div>
       </div>
     </section>
+    -->
   </main>
   <footer class="footer">
     <?php
